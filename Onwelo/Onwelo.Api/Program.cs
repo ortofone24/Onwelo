862bf1f9343ac53550a10c6e1631c5cf66376b71
MarcinGała <marcingala81@gimail.com>
2023-01-29 15:53:48 +0100

    Remove comment

--- Onwelo/Onwelo.Api/Program.cs
+++ Onwelo/Onwelo.Api/Program.cs
@@ -8,7 +8,6 @@ builder.Services.AddDatabaseFromExtensions()
     .AddIdentityServiceFromExtensions()
     .AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
